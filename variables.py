a59a1483f7cd6ffc784000c980b6028acccb2fb8
Maxkipkorir <77729472+Maxkipkorir@users.noreply.github.com>
2021-01-27 15:13:23 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+#A code to explain or describe variables
+#An interger assignment
+age= 21
+houses=5
+print(age , houses)
+score =80
+print(score)
+#Float point
+y=23.78
+print(y)
+weight=47
+print(weight)
+#string
+x="python variables"
+a=b=c=d=45.98
+print(a , b , c , d)
+
+print(x)
+
